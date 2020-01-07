39cdef0b025226d5abe4889c72a3e671b6a3ff19
Sorrel Bri <sorrel.bri.june@gmail.com>
2020-01-07 13:03:59 -0800

    stub basic connection

--- packages/server/src/App.js
+++ packages/server/src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.scss';
+import config from './config';
 
 function App() {
+  let fetchData = {};
+  fetch(config.apiAddress).then(res => {console.log(res);fetchData = res.body}).then(() => console.log(fetchData))
   return (
     <div className="App">
       <h1>React Boilerplate</h1>
+      {fetchData ? <p></p> : <></>}
     </div>
   );
 }
