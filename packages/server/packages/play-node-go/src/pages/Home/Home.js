cc6b00b72b985cf722ff11191d9c4d9cee3348a4
Sorrel Bri <sorrel.bri.june@gmail.com>
2020-01-08 17:17:10 -0800

    patch page function tests, import pages in App

--- packages/server/packages/play-node-go/src/pages/Home/Home.js
+++ packages/server/packages/play-node-go/src/pages/Home/Home.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import './Accounts.scss';
+import './Home.scss';
 
 const Home = () => {
   return (  
     <>
+      <p>Home</p>
     </>
   );
 }
--- packages/server/packages/play-node-go/src/pages/Room/Room.js
+++ packages/server/packages/play-node-go/src/pages/Room/Room.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import './Accounts.scss';
+import './Room.scss';
 
 const Room = () => {
   return (  
     <>
+      <p>Room</p>
     </>
   );
 }
