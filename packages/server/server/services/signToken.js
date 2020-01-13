323ea5d24cffbd9304e81eb31e795738fd307ea1
Sorrel Bri <sorrel.bri.june@gmail.com>
2020-01-13 14:47:45 -0800

    add sign jwt function and hook to auth/signup

---
+++ packages/server/server/services/signToken.js
@@ -0,0 +1,20 @@
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+const msDayOffset = 86400000;
+const msHourOffset = 3600000;
+
+const signToken = (res, user) => {
+  const expiration = process.env.NODE_ENV === 'test' ? msHourOffset : msDayOffset;
+  const secret = process.env.NODE_ENV === 'test' ? process.env.TEST_SECRET : process.env.JWT_SECRET;
+  console.log(process.env.NODE_ENV)
+  const token = jwt.sign({ user }, secret, {
+    expiresIn: process.env.NODE_ENV === 'test' ? '1h' : '24h',
+  });
+  return res.cookie('token', token, {
+    expires: new Date(Date.now() + expiration),
+    secure: false, // set to true if your using https
+    httpOnly: true,
+  });
+};
+module.exports = signToken;
