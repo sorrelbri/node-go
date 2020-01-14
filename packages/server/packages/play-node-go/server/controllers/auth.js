9a0045d99b3432aecd6fbfab84570a95cfa65c78
Sorrel Bri <sorrel.bri.june@gmail.com>
2020-01-14 12:06:25 -0800

    stub new user sign up

--- packages/server/packages/play-node-go/server/controllers/auth.js
+++ packages/server/packages/play-node-go/server/controllers/auth.js
@@ -1,11 +1,17 @@
+const knex = require('../data/db')
 const signToken = require('../services/signToken');
 
 const signUp = async (req, res, next) => {
   
   const user = req.body;
   try {
-    signToken(res, user);
-    res.send('ok').status(200);
+    knex('user')
+      .insert(user)
+      .then(newUser => {
+        signToken(res, newUser[0]);
+        res.send('ok').status(200);
+      }
+    )
   } 
   catch (err) {
     res.status(500).json(err)
