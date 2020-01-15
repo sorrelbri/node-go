98fdf6926c4dee2b90fc0cf6d72f7abcb7cf8e25
Sorrel Bri <sorrel.bri.june@gmail.com>
2020-01-14 23:09:01 -0800

    add login verification of user

--- packages/server/packages/play-node-go/server/controllers/auth.js
+++ packages/server/packages/play-node-go/server/controllers/auth.js
@@ -1,11 +1,14 @@
 const knex = require('../data/db')
+
 const { hashPassword, compareHash } = require('../services/bcrypt');
-
 const signToken = require('../services/signToken');
+const { validateSignup, validateLogin } = require('../services/userValidate');
 
-const signUp = async (req, res, next) => {
+const signup = async (req, res, next) => {
   
   const user = req.body;
+  if (!validateSignup(user)) return;
+  
   try {
     
     const hashedPassword = await hashPassword(user.password);
@@ -24,11 +27,38 @@ const signUp = async (req, res, next) => {
   }
 }
 
-const login = (req, res, next) => {
+const login = async (req, res, next) => {
+  
+  const user = req.body;
+  if (!validateLogin(user)) return;
 
+  try {
+
+    const queryResults = await knex('user')
+    .where({username: user.username})
+    .select('username', 'email', 'password')
+    .then(queryResults => queryResults);
+    
+    const savedUser = queryResults[0] || null;
+    if (!savedUser) return res.status(401).json({err: 'bad credentials'});
+    
+    const hashedPassword = savedUser.password;
+    const passwordMatch = await compareHash(user.password, hashedPassword);
+    
+    if (!passwordMatch) return res.status(401).json({err: 'bad credentials'});
+    
+    const authorizedUser = {...savedUser};
+    delete authorizedUser.password;
+    
+    signToken(res, authorizedUser);
+    res.send('ok').status(200);
+  }
+  catch (err) {
+    res.status(500).json(err);
+  }
 }
 
 module.exports = {
-  signUp,
+  signup,
   login
 }
