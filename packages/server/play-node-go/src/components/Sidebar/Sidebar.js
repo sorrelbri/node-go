9d10ed4fe2a518ee28fe43808eccd2f47570f287
Sorrel Bri <sorrel.bri.june@gmail.com>
2020-01-16 15:21:23 -0800

    stub styling of side and main content

--- packages/server/play-node-go/src/components/Sidebar/Sidebar.js
+++ packages/server/play-node-go/src/components/Sidebar/Sidebar.js
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import './Sidebar.scss';
 
 import AccountSidebar from '../../pages/Account/AccountSidebar';
-import GameSidebar from '../../pages/Game/GameSidebar';
 import HomeSidebar from '../../pages/Home/HomeSidebar';
 import NewsSidebar from '../../pages/News/NewsSidebar';
 import RoomSidebar from '../../pages/Room/RoomSidebar';
@@ -12,15 +11,13 @@ const Sidebar = (props) => {
   const displayComponent = props =>{
     switch (props.page) {
       case 'account':
-        return <AccountSidebar state={props.state}/>
-      case 'game':
-        return <GameSidebar state={props.state}/>
+        return <AccountSidebar state={props.state} dispatch={props.dispatch}/>
       case 'home':
-        return <HomeSidebar state={props.state}/>
+        return <HomeSidebar state={props.state} dispatch={props.dispatch}/>
       case 'news':
-        return <NewsSidebar state={props.state}/>
+        return <NewsSidebar state={props.state} dispatch={props.dispatch}/>
       case 'room':
-        return <RoomSidebar state={props.state}/>
+        return <RoomSidebar state={props.state} dispatch={props.dispatch}/>
       default:
         return <></>
     }
