e0a336bd35628e6f2058d6435cb5d6d6e85cfec1
Sorrel Bri <sorrel.bri.june@gmail.com>
2020-01-17 00:23:18 -0800

    patch signup post for existing user to return 409

--- packages/server/server/controllers/auth.js
+++ packages/server/server/controllers/auth.js
@@ -1,9 +1,9 @@
-const knex = require('../data/db')
-
-const { hashPassword, compareHash } = require('../services/bcrypt');
-const signToken = require('../services/signToken');
 
 const { validationResult } = require('express-validator');
+
+const userQueries = require('../data/queries/user');
+const { hashPassword, compareHash } = require('../services/bcrypt');
+const signToken = require('../services/signToken');
 
 const checkValidationErrors = (req, res) => {
   const errors = validationResult(req);
@@ -14,21 +14,22 @@ const checkValidationErrors = (req, res) => {
 
 const signup = async (req, res, next) => {
   checkValidationErrors(req, res);
-  
   const user = req.body;
+
   try {
     delete user.confirmPassword;
     const hashedPassword = await hashPassword(user.password);
-    const secureUser = { ...user, password: hashedPassword }
+    const secureUser = { ...user, password: hashedPassword };
+    const existingUser = await userQueries.findUserByNameOrEmail(secureUser);
 
-    knex('user')
-    .returning(['username', 'email'])
-    .insert(secureUser)
-    .then(queryResults => {
-      const newUser = queryResults[0];
-      signToken(res, newUser).send('ok').status(201);
-    })
+    if (existingUser.length) {
+      return res.status(409).json({errors: [{auth: 'User already exists!'}]})
+    }
+
+    const newUser = await userQueries.insertUser(secureUser)
+    signToken(res, newUser).status(201).json({...newUser});
   } 
+
   catch (err) {
     res.status(500).json(err);
   }
@@ -37,22 +38,22 @@ const signup = async (req, res, next) => {
 const login = async (req, res, next) => {
 
   checkValidationErrors(req, res);
-
   const user = req.body;
 
   try {
-
-    const queryResults = await knex('user')
-    .where({username: user.username})
-    .select()
-    .then(queryResults => queryResults);
-    
+    const queryResults = await userQueries.findUserByNameOrEmail(user);
     const savedUser = queryResults[0] || null;
-    if (!savedUser) return res.status(401).json({err: 'bad credentials'});
+
+    if (!savedUser) {
+      return res.status(401).json({err: 'bad credentials'});
+    }
     
     const hashedPassword = savedUser.password;
     const passwordMatch = await compareHash(user.password, hashedPassword);
-    if (!passwordMatch) return res.status(401).json({err: 'bad credentials'});
+
+    if (!passwordMatch) {
+      return res.status(401).json({err: 'bad credentials'});
+    }
     
     const authorizedUser = {...savedUser};
     delete authorizedUser.password;
@@ -60,6 +61,7 @@ const login = async (req, res, next) => {
     signToken(res, authorizedUser);
     res.send('ok').status(200);
   }
+  
   catch (err) {
     res.status(500).json(err);
   }
