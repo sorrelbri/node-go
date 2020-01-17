196b5e884bf4658f6b5acb4c362f815a72ef92fc
Sorrel Bri <sorrel.bri.june@gmail.com>
2020-01-17 00:23:18 -0800

    patch signup post  for existing user to return 409

---
+++ server/data/queries/user.js
@@ -0,0 +1,27 @@
+const knex = require('../db')
+
+const insertUser = async (user) => { 
+  return await knex('user')
+  .returning(['username', 'email'])
+  .insert(user)
+  .then(queryResults => {
+    newUser = queryResults[0];
+    return newUser
+  });
+}
+
+const findUserByNameOrEmail = async (user) => {
+  if (! user.email && !user.username) return [];
+  if (!user.email) user.email = '';
+  if (!user.username) user.username = '';
+
+  return await knex('user')
+  .where({'username': user.username})
+  .orWhere({'email': user.email})
+  .select(['username', 'email', 'password'])
+}
+
+module.exports = {
+  insertUser,
+  findUserByNameOrEmail
+}
