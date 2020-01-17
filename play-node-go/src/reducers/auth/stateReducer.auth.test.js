df110d187ab9d6243625740e941fd080f4581605
Sorrel Bri <sorrel.bri.june@gmail.com>
2020-01-16 16:59:11 -0800

    hook reducer into auth/signup request service

---
+++ play-node-go/src/reducers/auth/stateReducer.auth.test.js
@@ -0,0 +1,24 @@
+import {authReducer} from './stateReducer.auth';
+
+const newCredentials = {
+  username: 'newUsername7',
+  email: 'example7@test.com',
+  password: 'newPass1',
+  confirmPassword: 'newPass1'
+}
+
+const signupAction = {
+  body: newCredentials,
+  message: 'SIGNUP',
+  type: 'AUTH'
+}
+
+const state = {
+  user: null,
+  errors: {}
+}
+
+it('return ', async () => {
+  
+  
+})
--- play-node-go/src/reducers/stateReducer.js
+++ play-node-go/src/reducers/stateReducer.js
@@ -1,18 +1,30 @@
 // @flow
 import { initState } from './init/stateReducer.init';
+import { authReducer } from './auth/stateReducer.auth';
 
 export type state = {
-  user: {}
+  user: {},
+  errors: {}
 }
 
-type action = {
-  type: string
+export type action = {
+  type: string,
+  message: ?string,
+  body: {}
 }
 
 export const stateReducer = (state: state, action: action): state => {
+  const errorStrippedState = stripErrors({...state});
+
   switch (action.type) {
     case 'INIT': return initState();
+
+    case 'AUTH': return authReducer(errorStrippedState, action);
   
     default: return state;
   }
+}
+
+function stripErrors(state: state): state {
+  return {...state, errors: {}}
 }
