0ba2e2acdc1a334dd8b4a7f443ebfe2c672e67e6
Sorrel Bri <sorrel.bri.june@gmail.com>
2020-01-17 00:23:18 -0800

    patch signup post for existing user to return 409

--- packages/play-node-go/server/services/bcrypt.js
+++ packages/play-node-go/server/services/bcrypt.js
@@ -6,7 +6,6 @@ const saltRounds = process.env.NODE_ENV === 'test' ? 5 : parseInt(process.env.SA
 const hashPassword = async (password) => {
   const hashedPassword = await new Promise((resolve, reject) => {
     bcrypt.hash(password, saltRounds, (err, hash) => {
-      console.log(err)
       if (err) reject(err)
       resolve(hash)
     });
