8933e25156f234be4b55dea4ee8b88a20b2b0a52
Sorrel Bri <sorrel.bri.june@gmail.com>
2020-01-18 13:06:28 -0800

    patch component references in pages/

--- packages/server/play-node-go/src/pages/Layout/NavBar/NavBar.test.js
+++ packages/server/play-node-go/src/pages/Layout/NavBar/NavBar.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { render } from '@testing-library/react';
 import NavBar from './NavBar';
-import { initState } from '../../reducers/init/stateReducer.init';
+import { initState } from '../../../reducers/init/stateReducer.init';
 
 test('renders NavBar without crashing', () => {
   const { getByTestId } = render(
