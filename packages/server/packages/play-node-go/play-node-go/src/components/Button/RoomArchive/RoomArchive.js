b06b490d6f5597b24e021bf1ecfbb6d1b9764364
Sorrel Bri <sorrel.bri.june@gmail.com>
2020-01-18 12:20:33 -0800

    rename RoomArchive components to differentiate from NewsArchive

---
+++ packages/server/packages/play-node-go/play-node-go/src/components/Button/RoomArchive/RoomArchive.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './RoomArchive.scss';
+
+const RoomArchiveButton = () => {
+  return (
+    <>
+      Room Archive
+    </>
+  );
+}
+
+export default RoomArchiveButton;
