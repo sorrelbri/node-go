a6105973f34f3c516ace20a0517b698f052f3ade
Sorrel Bri <sorrel.bri.june@gmail.com>
2020-01-18 15:42:45 -0800

    stub rooms index route and controller

--- packages/play-node-go/server/routes/api.js
+++ packages/play-node-go/server/routes/api.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const apiIndexController = require('../controllers/api/apiIndex');
+const apiRoomRouter = require('./api/room');
+
+router.use('/rooms', apiRoomRouter);
 
 router.get('/', apiIndexController.apiIndex);
 
