b1cf43d84b1db5f22137fbd768374d3397c087de
Sorrel Bri <sorrel.bri.june@gmail.com>
2020-01-18 00:38:02 -0800

    patch config for production

--- packages/server/play-node-go/src/config.js
+++ packages/server/play-node-go/src/config.js
@@ -2,15 +2,17 @@ import runtimeEnv from '@mars/heroku-js-runtime-env';
 const env = runtimeEnv();
 
 const production = {
-  apiAddress: 'https://node-go-api.herokuapp.com',
-  endpoint: 'https://play-node-go.herokuapp.com'
+  apiAddress: 'https://node-go-api.herokuapp.com/api/v1',
+  endpoint: 'https://play-node-go.herokuapp.com',
+  authAddress: 'https://play-node-go.herokuapp.com',
+  socketAddress: 'https://node-go-api.herokuapp.com'
 }
 
 const development = {
-  authAddress: 'http://localhost:8000/auth',
   apiAddress: 'http://localhost:8000/api/v1',
-  socketAddress: 'http://localhost:8000',
-  endpoint: 'http://localhost:3000'
+  authAddress: 'http://localhost:8000/auth',
+  endpoint: 'http://localhost:3000',
+  socketAddress: 'http://localhost:8000'
 }
 
 const config = env.REACT_APP_ENVIRONMENT === 'production' 
