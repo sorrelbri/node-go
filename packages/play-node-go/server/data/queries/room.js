d81be0b595619e0b00bd796d69af42e2bdd83aca
Sorrel Bri <sorrel.bri.june@gmail.com>
2020-01-18 15:49:35 -0800

    serve all public rooms at rooms index

---
+++ packages/play-node-go/server/data/queries/room.js
@@ -0,0 +1,11 @@
+const knex = require('../db')
+
+const findPublicRooms = async () => {
+  return await knex('room')
+  .where('private', false)
+  .select(['id', 'name', 'description', 'language'])
+}
+
+module.exports = {
+  findPublicRooms
+}
