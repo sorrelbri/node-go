05b7f338ff4419294a01cbaa3a2452113f1ee758
Sorrel Bri <sorrel.bri.june@gmail.com>
2020-01-18 16:40:57 -0800

    hook SET_ROOMS into rooms Service

--- packages/server/packages/play-node-go/play-node-go/src/pages/Home/Home.js
+++ packages/server/packages/play-node-go/play-node-go/src/pages/Home/Home.js
@@ -1,7 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './Home.scss';
+import roomsServices from '../../services/api/roomsServices';
 
-const Home = () => {
+const Home = props => {
+
+  const fetchRoomsAPI = async () => {
+    const response = await roomsServices.indexService();
+    if (response) {
+      const action = {
+        type: 'ROOMS',
+        message: 'SET_ROOMS',
+        body: response
+      }
+      return props.dispatch(action)
+    }
+  }
+
+  useEffect(() => {
+    fetchRoomsAPI();
+  }, [])
+
   return (  
     <div className="page">
 
