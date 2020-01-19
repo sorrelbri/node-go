5a7f3bc1775c9fd8d20716038b2670ce31d3a418
Sorrel Bri <sorrel.bri.june@gmail.com>
2020-01-18 16:17:23 -0800

    add SET_ROOMS dispatch

---
+++ packages/server/play-node-go/src/reducers/rooms/stateReducer.rooms.js
@@ -0,0 +1,19 @@
+// @flow
+import type { state, action } from '../stateReducer';
+
+export const roomsReducer = (state: state, action: action):state => {
+  switch(action.message) {
+
+    case 'SET_ROOMS':
+      const rooms = roomsParse(action.body);
+      return {...state, rooms};
+      
+    default:
+      return state;
+  }
+}
+
+function roomsParse(roomsData) {
+  const rooms = JSON.parse(roomsData);
+  return rooms
+}
