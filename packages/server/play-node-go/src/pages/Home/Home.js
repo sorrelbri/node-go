5867c0ddd9c13c58b4c633ac4801981beb9047c3
Sorrel Bri <sorrel.bri.june@gmail.com>
2020-01-21 14:33:40 -0800

    stub room socket connection (client + server)

--- packages/server/play-node-go/src/pages/Home/Home.js
+++ packages/server/play-node-go/src/pages/Home/Home.js
@@ -2,11 +2,18 @@ import React, { useEffect } from 'react';
 import './Home.scss';
 import roomsServices from '../../services/api/roomsServices';
 import RoomButton from '../../components/Button/Room/Room';
+import socketIOClient from 'socket.io-client';
+import config from '../../config';
+
+// ! [start] roomSocket stub
+const roomId = 1
+const roomSocket = socketIOClient(`${config.socketAddress}/${roomId}`)
+// ! [end]
 
 const Home = props => {
-  const state =     props.state;
+  const state =     props.state || {};
   const dispatch =  props.dispatch;
-
+  
   const renderRooms = () => {
     const rooms = state.rooms || [];
     if (rooms.length) {
@@ -31,10 +38,23 @@ const Home = props => {
       return dispatch(action)
     }
   }
-
+  
   useEffect(() => {
     fetchRoomsAPI();
   }, [])
+
+  // ! [start] roomSocket stub
+  const roomSocketConnect = () => {
+    roomSocket.emit('connect');
+    // roomSocket.on('connected', data => setSocketData('room socket connected'));
+    // roomSocket.on('connect_error', err => setError([...error, err]));
+    // roomSocket.on('error', err => setError([...error, err]))
+  }
+
+  useEffect(() => {
+    roomSocketConnect();
+  }, [])
+  // ! [end]
 
   return (  
     <div className="page">
--- packages/server/server/controllers/api/apiRoom.js
+++ packages/server/server/controllers/api/apiRoom.js
@@ -1,9 +1,11 @@
 const roomQueries = require('../../data/queries/room');
+const {enableRoomSocket} = require('../../socket');
 
 const roomIndex = async (req, res, next) => {
   try {
     // TODO eventually add check for user's private rooms
     const publicRooms = await roomQueries.findPublicRooms();
+    enableRoomSocket(1)
     res.status(200).json({rooms: publicRooms})
   }
 
