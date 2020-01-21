c323968404af42221019d73c48aff2d1b48f65db
Sorrel Bri <sorrel.bri.june@gmail.com>
2020-01-21 14:33:40 -0800

    stub room socket connection (client + server)

--- server/socket.js
+++ server/socket.js
@@ -1,13 +1,20 @@
 // TODO const someSocketLogic = require('./middleware/socketssockets/...');
+const socketIO = require('socket.io');
+const io = socketIO({ cookie: false });
+
+io.on('connection', ()=> {
+  io.emit('connected', {message: 'socket connected'});
+})
+
+enableRoomSocket = (roomId) => {
+  const roomSocket = io.of(roomId);
+  roomSocket.on('connection', () => {
+    console.log(`Socket connected at room ${roomId}`);
+  });
+  return roomSocket;
+}
 
 module.exports = {
-  enableSocket: io => {
-    io.on('connection', ()=> {
-      io.emit('connected', {message: 'socket connected'});
-    })
-
-    // TODO io = someSocketLogic(io);
-
-    return io;
-  }
+  io,
+  enableRoomSocket
 }
