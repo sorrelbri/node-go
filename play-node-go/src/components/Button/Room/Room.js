833d8fa1ddabb15db61fbde167ab5bca4a23a2c7
Sorrel Bri <sorrel.bri.june@gmail.com>
2020-01-22 11:43:36 -0800

    add client routing to rooms/:id, move socket(rooms/roomId) connect from Home page to Room page

--- play-node-go/src/components/Button/Room/Room.js
+++ play-node-go/src/components/Button/Room/Room.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Room.scss';
 
 const RoomButton = (props) => {
@@ -6,6 +7,7 @@ const RoomButton = (props) => {
   return (
     <div className="RoomButton" data-testid="RoomButton">
       <h4 className="RoomButton__room-name">{roomData.name}</h4>
+      <Link to={`/rooms/${roomData.id}`}>Join Room</Link>
     </div>
   );
 }
