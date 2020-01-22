c992bf4b4f08723fd8a49d710f3cd7d948d365c7
Sorrel Bri <sorrel.bri.june@gmail.com>
2020-01-21 16:01:35 -0800

    serve room with joined game data from api/v1/rooms/:id

---
+++ packages/play-node-go/server/data/seeds/03_game.js
@@ -0,0 +1,17 @@
+
+exports.seed = function(knex) {
+  // Deletes ALL existing entries
+  return knex('game').del()
+    .then(function () {
+      // Inserts seed entries
+      return knex('game').insert([
+        { 
+          id: 1, date: new Date(), 
+          application: 'node-go', application_version: '0.1.0', 
+          player_black: 'anon', player_white: 'anon',
+          player_black_rank: 'K3', player_white_rank: 'K2',
+          room: 1, time_setting: 1
+        }
+      ]);
+    });
+};
--- packages/play-node-go/server/routes/api/room.js
+++ packages/play-node-go/server/routes/api/room.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const apiRoomController = require('../../controllers/api/apiRoom');
 
-router.get('/', apiRoomController.roomIndex);
+router.get('/', apiRoomController.getAll);
+router.get('/:id', apiRoomController.show);
 
 module.exports = router;
