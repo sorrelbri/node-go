ebd76baac6a8275bcd9af48de786239274413df2
Sorrel Bri <sorrel.bri.june@gmail.com>
2020-01-22 15:41:08 -0800

    add SET_MESSAGES to reducer

--- packages/server/play-node-go/src/reducers/messages/stateReducer.messages.test.js
+++ packages/server/play-node-go/src/reducers/messages/stateReducer.messages.test.js
@@ -1,10 +1,20 @@
 import {stateReducer} from '../stateReducer';
 import { initState } from '../init/stateReducer.init';
 
-const messagesData = [];
+const messagesData = [
+  {
+  "content": "Hey! Welcome to the general room!", "username": "userOne", "admin": true
+  }
+];
 
 it('default returns state unaltered', () => {
   const state = initState();
-  const action = {type: 'MESSAGES', message: '', body: JSON.stringify(messagesData)};
+  const action = {type: 'MESSAGES', message: '', body: messagesData};
   expect(stateReducer(state, action)).toEqual(state);
+})
+
+it('default returns state unaltered', () => {
+  const state = initState();
+  const action = {type: 'MESSAGES', message: 'SET_MESSAGES', body: messagesData};
+  expect(stateReducer(state, action)).toEqual({...state, messages: messagesData});
 })
