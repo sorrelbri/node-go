b3d05639ad1acc6f02c7f20188fcc720d4d55c4b
Sorrel Bri <sorrel.bri.june@gmail.com>
2020-01-21 16:01:35 -0800

    serve room with joined game data from api/v1/rooms/:id

--- packages/server/server/test/room/api.room.spec.js
+++ packages/server/server/test/room/api.room.spec.js
@@ -1,6 +1,21 @@
 const apiRoomSpec = (chai, knex, server) => {
   const roomEndpoint = '/api/v1/rooms';
   const publicRooms = {rooms: [{id: 1, name: 'main', description: 'A general place to play Go', language: 'EN'}]};
+  const roomOne = {
+    roomGames: [ {
+      id: 1,
+      name: 'main',
+      description: 'A general place to play Go',
+      language: 'EN',
+      komi: 6.5,
+      handicap: 0,
+      board_size: 19,
+      player_black: 'anon',
+      player_white: 'anon',
+      player_black_rank: 'K3',
+      player_white_rank: 'K2'
+    } ]
+  }
   
   it('seeded rooms should be present in db', done => {
     knex('room').where('id', 1).orWhere('id', 2).select('name').then(roomResults => {
@@ -24,6 +39,16 @@ const apiRoomSpec = (chai, knex, server) => {
     .end((err,res)=> {
       if(err) done(err);
       res.body.should.eql(publicRooms);
+      done();
+    });
+  })
+
+  it('request to api room/1 should return 1 room record with game and message information', done => {
+    chai.request(server)
+    .get(`${roomEndpoint}/1`)
+    .end((err,res)=> {
+      if(err) done(err);
+      res.body.should.eql(roomOne);
       done();
     });
   })
