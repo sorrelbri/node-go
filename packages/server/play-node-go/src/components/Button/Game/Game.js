d7c024e286086c59add2ec56f16f175b6391e3fe
Sorrel Bri <sorrel.bri.june@gmail.com>
2020-01-23 21:41:25 -0800

    refactor join_game_request to require unique users

--- packages/server/play-node-go/src/components/Button/Game/Game.js
+++ packages/server/play-node-go/src/components/Button/Game/Game.js
@@ -3,12 +3,53 @@ import { Link } from 'react-router-dom';
 import './Game.scss';
 
 const GameButton = (props) => {
-  const gameData = props.game;
+  const { game, dispatch } = props;
+
+  const requestJoinGame = () => {
+    console.log(`request to Join Game ${game.id}!`)
+    const requestAction = {
+      type: 'GAMES',
+      message: 'JOIN_REQUEST',
+      body: {id: game.id}
+    }
+    dispatch(requestAction);
+  }
+
+  const renderOpenGame = () => {
+    return (
+      <>
+        <a onClick={() => requestJoinGame()} >Request to Join Game</a>
+
+        <div className="Game__playerData">
+          <span className="Game__playerData__name">{game.playerBlack}</span>
+          <span className="Game__playerData__rank">{game.playerBlackRank}</span>
+        </div>
+      </>
+    )
+  }
+
+  const renderInProgressGame = () => {
+    const gameLinkText = game.winType ? 'Study Game' : 'Watch Game'
+    return (
+      <>
+        <Link to={`/games/${game.id}`}>{gameLinkText}</Link>
+        
+        <div className="Game__playerData">
+          <span className="Game__playerData__name">{game.playerBlack}</span>
+          <span className="Game__playerData__rank">{game.playerBlackRank}</span>
+        </div>
+
+        <div className="Game__playerData">
+          <span className="Game__playerData__name">{game.playerWhite}</span>
+          <span className="Game__playerData__rank">{game.playerWhiteRank}</span>
+        </div>
+      </>
+    )
+  }
+
   return (
     <div className="GameButton" data-testid="GameButton">
-      <Link to={`/games/${gameData.id}`}>View Game</Link>
-      <p>{gameData.playerBlack} - {gameData.playerBlackRank}</p>
-      <p>{gameData.playerWhite} - {gameData.playerWhiteRank}</p>
+      {game.open ? renderOpenGame() : renderInProgressGame()}
     </div>
   );
 }
