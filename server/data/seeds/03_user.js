680bb413372da1c5188d2b57ba611e802ed89ef8
Sorrel Bri <sorrel.bri.june@gmail.com>
2020-01-23 21:25:08 -0800

    connect join_game_request socket at client and server

--- server/data/seeds/03_user.js
+++ server/data/seeds/03_user.js
@@ -1,16 +1,18 @@
 const {hashPassword} = require('../../services/bcrypt');
 require('dotenv').config();
+
 const password = process.env.USER_ONE_PASSWORD;
-const hashedPassword = hashPassword(password);
 const email = process.env.USER_ONE_EMAIL;
 
-exports.seed = function(knex) {
+exports.seed = async function(knex) {
   // Deletes ALL existing entries
   return knex('user').del()
-    .then(function () {
+  .then(async function () {
+      const hashedPassword = await hashPassword(password);
       // Inserts seed entries
       return knex('user').insert([
         {id: 2, username: 'user-one', email: email, password: hashedPassword, admin: true},
+        {id: 3, username: 'user-two', email: `2${email}`, password: hashedPassword, admin: true},
       ]);
     });
 };
--- server/socket.js
+++ server/socket.js
@@ -2,14 +2,21 @@
 const socketIO = require('socket.io');
 const io = socketIO({ cookie: false });
 
+const gameQueries = require('./data/queries/game');
+
 io.on('connection', ()=> {
   io.emit('connected', {message: 'socket connected'});
 })
 
 enableRoomSocket = (roomId) => {
   const roomSocket = io.of(roomId);
-  roomSocket.on('connection', () => {
+  roomSocket.on('connection', (socket) => {
+    // socket.emit('connected');
     console.log(`Socket connected at room ${roomId}`);
+    socket.on('join_game_request', async data => {
+      const gameRequest = await logJoinGameRequest(data);
+      roomSocket.emit('join_game_request', gameRequest);
+    })
   });
   return roomSocket;
 }
@@ -17,4 +24,10 @@ enableRoomSocket = (roomId) => {
 module.exports = {
   io,
   enableRoomSocket
+}
+
+async function logJoinGameRequest (data) {
+  const {user, game} = data;
+  const requestedGame = await gameQueries.findGameById(game.id);
+  return { user, requestedGame }
 }
