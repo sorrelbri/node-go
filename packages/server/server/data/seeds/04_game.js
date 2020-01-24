e3ce466bb43317444da3d084839bab9f7d576b93
Sorrel Bri <sorrel.bri.june@gmail.com>
2020-01-23 16:04:40 -0800

    patch no-query bug in rooms/ endpoint

--- packages/server/server/data/seeds/04_game.js
+++ packages/server/server/data/seeds/04_game.js
@@ -10,7 +10,14 @@ exports.seed = function(knex) {
           application: 'node-go', application_version: '0.1.0', 
           player_black: 'anon', player_white: 'anon',
           player_black_rank: 'K3', player_white_rank: 'K2',
-          room: 1, time_setting: 1
+          room: 1, time_setting: 1, open: false
+        },
+        { 
+          id: 2, date: new Date(), 
+          application: 'node-go', application_version: '0.1.0', 
+          player_black: 'user-one', player_black_rank: 'UR',
+          user_black: 2,
+          room: 1, time_setting: 1, open: true
         }
       ]);
     });
