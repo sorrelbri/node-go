61b13ab97a276e24491a1865aef2c974b605ef3a
Sorrel Bri <sorrel.bri.june@gmail.com>
2020-01-25 16:50:18 -0800

    refacor sockets on server and client to utilize dispatch for socket meessages

--- packages/server/packages/play-node-go/server/controllers/api/apiGame.js
+++ packages/server/packages/play-node-go/server/controllers/api/apiGame.js
@@ -2,15 +2,14 @@ const roomQueries = require('../../data/queries/room');
 const messageQueries = require('../../data/queries/message');
 const gameQueries = require('../../data/queries/game');
 const moveQueries = require('../../data/queries/move');
-const { enableGameSocket } = require('../../socket');
 
 const show = async (req, res, next) => {
   try {
     const gameId = req.params.id;
     if (!gameId) throw('missing game parameter')
-    const game = await gameQueries.findGameById(gameId);
-    enableGameSocket(game.room);
     
+    // TODO Promise.all()
+    const game = await gameQueries.findGameById(gameId);    
     const record = await moveQueries.findGameRecord(gameId);
     res.status(200).json({game, record})
   }
