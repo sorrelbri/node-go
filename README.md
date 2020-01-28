d7c0477c88cf026adbab2e9351da998f5e6ed377
Sorrel Bri <sorrel.bri.june@gmail.com>
2020-01-28 14:33:54 -0800

    debug room socket connect dispatch

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-[Visit site](https://play-node-go.herokuapp.com/)
+# Node Go
+A browser application to play Go in real time.
+
+## Development Demo
+[The project in it's current state](https://play-node-go.herokuapp.com/)
+
+## Features
+- [ ] Realtime communications
+- [ ] Multiple game settings
+- [ ] Customizable board size
+
+
+## Built with
+- [Express](https://expressjs.com)
+- [React](https://reactjs.org)
+- [PostgreSQL](https://postgresql.org)
+- [Socket.io](https://socket.io)
+- [Sass](https://sass-lang.com)
--- play-node-go/src/io.js
+++ play-node-go/src/io.js
@@ -3,7 +3,7 @@ import config from './config';
 
 const launch = (nsp, dispatch) => {
   const socket = socketIOClient(`${config.socketAddress}/${nsp}`);
-
+  
   socket.on('connected', () => {
     dispatch({ type:'SOCKET', message:'CONNECTED', body:{nsp: socket.nsp} });
   });
@@ -13,14 +13,14 @@ const launch = (nsp, dispatch) => {
   socket.on('error', err => {
     dispatch({ type: 'ERR', message:'SOCKET_ERROR', body: { socketError: err } });
   });
-
+  
   socket.on('room_connected', (data) => {
-    dispatch({ type: 'ROOM', message: 'CONNECT_ROOM', body: data });
+    dispatch({ type: 'ROOMS', message: 'CONNECT_ROOM', body: data });
   });
-
+  
   socket.on('new_user', (data) => {
     console.log('new_user received')
-    dispatch({ type: 'ROOM', message: 'NEW_USER', body: data })
+    dispatch({ type: 'ROOMS', message: 'NEW_USER', body: data })
   })
 
   return socket;
