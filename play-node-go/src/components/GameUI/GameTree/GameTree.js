5ef4d390ff57503b4e09d7730305bd05df177de1
Sorrel Bri <sorrel.bri.june@gmail.com>
2020-01-30 00:30:56 -0800

    stub Game components

---
+++ play-node-go/src/components/GameUI/GameTree/GameTree.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './GameTree.scss';
+
+const GameTree = () => {
+  return (
+    <div className="GameTree">
+      
+    </div>
+  );
+}
+
+export default GameTree;
