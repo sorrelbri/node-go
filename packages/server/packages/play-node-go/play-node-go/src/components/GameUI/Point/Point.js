4851f31744159063d45f396a87f9a70d9f1f411d
Sorrel Bri <sorrel.bri.june@gmail.com>
2020-01-30 15:56:50 -0800

    stub board on game page

--- packages/server/packages/play-node-go/play-node-go/src/components/GameUI/Point/Point.js
+++ packages/server/packages/play-node-go/play-node-go/src/components/GameUI/Point/Point.js
@@ -3,9 +3,21 @@ import './Point.scss';
 
 const Point = (props) => {
   const { posX, posY, user, game, record } = props;
+  const xFlag = () => {
+    if ( posX === 1 ) return `board__point--top`
+    if ( posX === game.boardSize ) return `board__point--bottom`
+    return '';
+  }
+  const yFlag = () => {
+    if ( posY === 1 ) return `board__point--left`
+    if ( posY === game.boardSize ) return `board__point--right`
+    return '';
+  }
 
   return (
-    <></>
+    <div className={`board__point ${xFlag()} ${yFlag()}`}>
+
+    </div>
   );
 }
 
