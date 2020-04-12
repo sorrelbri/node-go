a9025aca8fc85ae9eb9d28bb5342eedcfcfd59d0
sorrelbri <sorrel.bri.june@gmail.com>
2020-04-11 21:39:09 -0700

    style small Room button

--- packages/play-node-go/src/components/Button/Room/Room.js
+++ packages/play-node-go/src/components/Button/Room/Room.js
@@ -2,12 +2,61 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './Room.scss';
 
-const RoomButton = (props) => {
-  const roomData = props.room;
+const RoomButton = ({room}) => {
+  const smallRoom = (
+    <div className="small-room">
+      <div className="small-room__roof">
+        <div className="small-room__roof-top small-room__roof-top--left"></div>
+        <div className="small-room__roof--left"></div>
+        <div className="small-room__roof-top small-room__roof-top--right"></div>
+        <div className="small-room__roof--right"></div>
+      </div>
+      <div className="small-room__body">
+        <div className="small-room__trim small-room__trim--left"></div>
+        <div className="small-room__sign">
+          <div className="small-room__sign__stone small-room__sign__stone--black"></div>
+          <div className="small-room__sign__stone small-room__sign__stone--white"></div>
+          <div className="small-room__sign__stone small-room__sign__stone--white"></div>
+          <div className="small-room__sign__stone small-room__sign__stone--black"></div>
+        </div>
+        <div className="small-room__window small-room__window--left">
+          <div className="small-room__window-frame small-room__window-frame--top"></div>
+          <div className="small-room__window-frame small-room__window-frame--left"></div>
+          <div className="small-room__window-frame small-room__window-frame--middle"></div>
+          <div className="small-room__window-frame small-room__window-frame--center"></div>
+          <div className="small-room__window-frame small-room__window-frame--right"></div>
+          <div className="small-room__window-frame small-room__window-frame--bottom"></div>
+        </div>
+        <div className="small-room__door">
+          <div className="small-room__door-frame small-room__door-frame--top"></div>
+          <div className="small-room__door-frame small-room__door-frame--left"></div>
+          <div className="small-room__door-handle"></div>
+          <div className="small-room__door-frame small-room__door-frame--right"></div>
+        </div>
+        <div className="small-room__window small-room__window--right">
+          <div className="small-room__window-frame small-room__window-frame--top"></div>
+          <div className="small-room__window-frame small-room__window-frame--left"></div>
+          <div className="small-room__window-frame small-room__window-frame--middle"></div>
+          <div className="small-room__window-frame small-room__window-frame--center"></div>
+          <div className="small-room__window-frame small-room__window-frame--right"></div>
+          <div className="small-room__window-frame small-room__window-frame--bottom"></div>
+        </div>
+        <div className="small-room__trim small-room__trim--bottom-left"></div>
+        <div className="small-room__trim small-room__trim--right"></div>
+        <div className="small-room__trim small-room__trim--bottom-right"></div>
+      </div>
+    </div>  
+  )
+
   return (
     <div className="RoomButton" data-testid="RoomButton">
-      <h4 className="RoomButton__room-name">{roomData.name}</h4>
-      <Link to={`/rooms/${roomData.id}`}>Join Room</Link>
+      <h4 className="RoomButton__room-link RoomButton__room-link--action">
+        <Link to={`/rooms/${room.id}`}>Join {room.name}</Link>
+      </h4>
+      <h4 className="RoomButton__room-link RoomButton__room-link--info">
+        ?
+      </h4>
+      {smallRoom}
     </div>
   );
 }
