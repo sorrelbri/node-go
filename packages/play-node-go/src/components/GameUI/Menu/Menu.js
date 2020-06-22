62fe6dfcb4ebf2705b17d8b462ea99f2ba540684
sorrelbri <sorrel.bri.june@gmail.com>
2020-06-21 18:47:00 -0700

    add click handler and basic style to menu on Game

--- packages/play-node-go/src/components/GameUI/Menu/Menu.js
+++ packages/play-node-go/src/components/GameUI/Menu/Menu.js
@@ -1,7 +1,22 @@
 import React from "react";
+import "./Menu.scss";
 
-const Menu = ({ ...props }) => {
-  return <></>;
+const Menu = ({ showMenu, clickClose, ...props }) => {
+  const handleBackgroundClick = (e) => {
+    if (e.target.className === "Game__Menu-container") clickClose();
+  };
+
+  if (!showMenu) return <></>;
+  return (
+    <div
+      className="Game__Menu-container"
+      onClick={(e) => handleBackgroundClick(e)}
+    >
+      <div className="Game__Menu-container__Menu">
+        <button onClick={clickClose}>X</button>
+      </div>
+    </div>
+  );
 };
 
 export default Menu;
