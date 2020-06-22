f14c0ce08703c6b7e0e69ee0a7949e2a4e9a16e0
sorrelbri <sorrel.bri.june@gmail.com>
2020-06-21 17:34:03 -0700

    style Kifu to hover 'show menu' text

--- packages/play-node-go/src/components/GameUI/Kifu/Kifu.js
+++ packages/play-node-go/src/components/GameUI/Kifu/Kifu.js
@@ -2,8 +2,15 @@ import React from "react";
 import "./Kifu.scss";
 
 const Kifu = () => {
+  const handleClick = (e) => {
+    console.log("clicked");
+  };
+
   return (
     <div className="Kifu">
+      <p className="Kifu__show-menu" onClick={handleClick}>
+        Show Menu?
+      </p>
       <div className="Kifu__board"></div>
     </div>
   );
