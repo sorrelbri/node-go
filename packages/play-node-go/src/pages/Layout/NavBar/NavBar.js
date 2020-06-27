9cae5ff1856283319be9544bda34904d3b76e68c
sorrelbri <sorrel.bri.june@gmail.com>
2020-06-27 13:05:38 -0700

    style Continue as Guest button, patch account display on NavBar

--- packages/play-node-go/src/pages/Layout/NavBar/NavBar.js
+++ packages/play-node-go/src/pages/Layout/NavBar/NavBar.js
@@ -1,29 +1,36 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './NavBar.scss';
-import Logo from '../../../components/Display/Logo/Logo';
+import React from "react";
+import { Link } from "react-router-dom";
+import "./NavBar.scss";
+import Logo from "../../../components/Display/Logo/Logo";
 
-const NavBar = (props) => {
-
+const NavBar = ({ state }) => {
   return (
     <div className="NavBar" data-testid="NavBar">
-      <Link to="/home" >
-        <div className="NavBar__logo"><Logo /></div>
+      <Link to="/home">
+        <div className="NavBar__logo">
+          <Logo />
+        </div>
       </Link>
 
-      <Link to="/home" >
-        <div className="NavBar__menu-item NavBar__home"><p className="--link">Find a Game</p></div>
+      <Link to="/home">
+        <div className="NavBar__menu-item NavBar__home">
+          <p className="--link">Find a Game</p>
+        </div>
       </Link>
-      
+
       <Link to="/news">
-        <div className="NavBar__menu-item NavBar__news"><p className="--link">News</p></div>
+        <div className="NavBar__menu-item NavBar__news">
+          <p className="--link">News</p>
+        </div>
       </Link>
 
       <Link to="/account">
-        <div className="NavBar__menu-item NavBar__account">{props.user ? props.user.username : <></>}</div>
+        <div className="NavBar__menu-item NavBar__account">
+          {state.user.username ? state.user.username : <></>}
+        </div>
       </Link>
     </div>
   );
-}
+};
 
 export default NavBar;
