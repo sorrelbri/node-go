5eca1281108d171e6bcc606cb6a95939f0ba8377
sorrelbri <sorrel.bri.june@gmail.com>
2020-07-01 10:54:32 -0700

    add placement boolean to Move migration, add placement and prior_move selects to move query

--- packages/server/data/migrations/20200120230921_move.js
+++ packages/server/data/migrations/20200120230921_move.js
@@ -1,17 +1,18 @@
-const players = ['white', 'black']
+const players = ["white", "black"];
 
-exports.up = knex => {
-  return knex.schema.createTable("move", table => {
-    table.increments('id').primary();
-    table.enu('player', players).notNullable();
-    table.integer('point_x').notNullable();
-    table.integer('point_y').notNullable();
-    table.integer('number').notNullable();
-    table.boolean('game_record').notNullable().default(true);
+exports.up = (knex) => {
+  return knex.schema.createTable("move", (table) => {
+    table.increments("id").primary();
+    table.enu("player", players).notNullable();
+    table.integer("point_x").notNullable();
+    table.integer("point_y").notNullable();
+    table.integer("number").notNullable();
+    table.boolean("game_record").notNullable().default(true);
+    table.boolean("placement").notNullable().default(false);
 
-    table.integer('game').references('id').inTable('game').notNullable();
-    table.integer('prior_move').references('id').inTable('move');
+    table.integer("game").references("id").inTable("game").notNullable();
+    table.integer("prior_move").references("id").inTable("move");
   });
 };
 
-exports.down = knex => knex.schema.dropTableIfExists("move");
+exports.down = (knex) => knex.schema.dropTableIfExists("move");
